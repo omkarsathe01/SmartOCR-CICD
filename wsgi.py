bc73ba68f9e43d74d7e65bfef71d78e975b27a3a
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-06-07 12:36:16 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from TextfromDoc import app as application
-
-
+from prometheus_client import start_http_server
 
 if __name__ == "__main__":
+    start_http_server(8000)
     application.run(port=5000)
