e88f79adf04dce88da986cd672398c7b1a380df7
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-06-07 13:20:39 +0530

    Update TextfromDoc.py

--- TextfromDoc.py
+++ TextfromDoc.py
@@ -32,7 +32,9 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 @app.route("/")
+@REQUEST_LATENCY.time()
 def home():
+    REQUEST_COUNT.labels(endpoint='/').inc()
     return "<h1>SMART OCR EXTRACTION </h1>"
 
 @app.route("/TextFromDoc", methods=['POST'])
